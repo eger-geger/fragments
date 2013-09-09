e613085e7da1fb66d31c4e045a4a85ed322d4645
eger <edongrossi1@gmail.com>
2013-09-09 23:47:49 +0300

    - locator parsers added

---
+++ src/main/java/com/astound/fragments/locators/parsers/StringLocatorParser.java
@@ -0,0 +1,57 @@
+package com.astound.fragments.locators.parsers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ByIdOrName;
+import org.openqa.selenium.support.How;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringLocatorParser implements LocatorParser<String> {
+
+	private final static Pattern CUSTOM_LOCATOR_PATTERN = Pattern
+			.compile("(css|xpath|id|name|tag_name|id_or_name|class_name|link_text|partial_link_text)\\s*::\\s*(.+)", Pattern.CASE_INSENSITIVE);
+
+	@Override public By parse(String findByString) {
+		Matcher matcher = CUSTOM_LOCATOR_PATTERN.matcher(findByString);
+
+		if (matcher.matches()) {
+			return buildBy(howValueOf(matcher), locatorValueOf(matcher));
+		}
+
+		throw new IllegalArgumentException(String.format("Incorrect syntax [%s].", findByString));
+	}
+
+	private static String locatorValueOf(Matcher matcher) {
+		return matcher.group(2).trim();
+	}
+
+	private static How howValueOf(Matcher matcher) {
+		return Enum.valueOf(How.class, matcher.group(1).trim().toUpperCase());
+	}
+
+	private static By buildBy(How how, String locator) {
+		switch (how) {
+			case CLASS_NAME:
+				return By.className(locator);
+			case CSS:
+				return By.cssSelector(locator);
+			case ID:
+				return By.id(locator);
+			case ID_OR_NAME:
+				return new ByIdOrName(locator);
+			case LINK_TEXT:
+				return By.linkText(locator);
+			case NAME:
+				return By.name(locator);
+			case PARTIAL_LINK_TEXT:
+				return By.partialLinkText(locator);
+			case TAG_NAME:
+				return By.tagName(locator);
+			case XPATH:
+				return By.xpath(locator);
+			default:
+				throw new IllegalArgumentException("Cannot determine how to locate element " + how);
+		}
+	}
+}
